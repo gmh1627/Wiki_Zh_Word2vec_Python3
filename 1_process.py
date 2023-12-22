4a3c88753b45e9f6cd641fb1c23be8b8f2b6e052
KangarooGao <3558965271@qq.com>
2023-12-22 15:29:05 +0800

    1_process.py

--- 1_process.py
+++ 1_process.py
@@ -1,14 +1,26 @@
-#!/usr/bin/env python
-# -*- coding: utf-8  -*-
 #将xml的wiki数据转换为text格式
+
+"""
+This script converts XML wiki data to text format.
+"""
 
 import logging
 import os.path
 import sys
 
 from gensim.corpora import WikiCorpus
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+import nltk
+
+def lemmatize(text, tokens, lemmatize, lowercase):
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(word) for word in word_tokenize(text)]
 
 if __name__ == '__main__':
+    nltk.download('wordnet')
+    nltk.download('omw-1.4')
+    
     program = os.path.basename(sys.argv[0])#得到文件名
     logger = logging.getLogger(program)
 
@@ -17,15 +29,15 @@ if __name__ == '__main__':
     logger.info("running %s" % ' '.join(sys.argv))
 
     if len(sys.argv) < 3:
-        print globals()['__doc__'] % locals()
+        print (globals()['__doc__'] % locals())
         sys.exit(1)
 
     inp, outp = sys.argv[1:3]
     space = " "
     i = 0
 
-    output = open(outp, 'w')
-    wiki =WikiCorpus(inp, lemmatize=False, dictionary=[])#gensim里的维基百科处理类WikiCorpus
+    output = open(outp, 'w', encoding='utf-8')
+    wiki =WikiCorpus(inp, tokenizer_func=lemmatize, dictionary=[])#gensim里的维基百科处理类WikiCorpus
     for text in wiki.get_texts():#通过get_texts将维基里的每篇文章转换位1行text文本，并且去掉了标点符号等内容
         output.write(space.join(text) + "\n")
         i = i+1
@@ -34,7 +46,3 @@ if __name__ == '__main__':
 
     output.close()
     logger.info("Finished Saved "+str(i)+" articles.")
-    
-
-
-
